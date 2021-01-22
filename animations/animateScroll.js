544207356c3dfc7363f7434517654f7b1e8ac48a
Ronaldo Michuy Valera <66487844+Website14-rnmv@users.noreply.github.com>
2021-01-21 22:18:00 -0500

    Add files via upload

---
+++ animations/animateScroll.js
@@ -0,0 +1,111 @@
+window.sr = ScrollReveal()
+
+/*---------------------------INICIO DE CABECERA Y NAV--------------------------*/
+sr.reveal('.menu',{
+    duration : 1500,
+    origin : 'top',
+    distance : '200px'
+
+});    
+
+
+sr.reveal('.content_redes',{
+        duration : 1500,
+        origin : 'bottom',
+        distance : '200px'
+
+});
+
+sr.reveal('.content_logo',{
+    duration : 1500,
+    origin : 'top',
+    distance : '200px'
+
+});
+
+sr.reveal('.titulo',{
+    duration : 3000,
+    origin : 'left',
+    distance : '200px'
+
+});
+
+sr.reveal('.bolas',{
+    duration : 3000,
+    origin : 'right',
+    distance : '200px'
+
+});
+
+/*---------------------------INFORMACION--------------------------*/
+
+    
+sr.reveal('.wid',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '200px'
+
+});
+
+/*---------------------------STOCK--------------------------*/
+
+    
+sr.reveal('.stock_text',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '200px'
+
+});
+
+/*---------------------------PROMOCIONES--------------------------*/
+
+    
+sr.reveal('.cronograma',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '200px'
+
+});
+
+/*---------------------------FOOTER--------------------------*/
+
+    
+sr.reveal('.animateFoot',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '200px'
+
+});
+    
+/*---------------------------ANIMACIONES DE LA TIENDAs--------------------------*/
+
+sr.reveal('.texto',{
+    duration : 2000,
+    origin : 'top',
+    distance : '200px'
+
+});
+
+sr.reveal('.content_productos',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '200px'
+
+});
+
+/*---------------------------ANIMACIONES DE CONTACTO--------------------------*/
+
+    
+sr.reveal('.wid',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '200px'
+
+});
+
+sr.reveal('.content_form',{
+    duration : 2000,
+    origin : 'bottom',
+    distance : '200px'
+
+});
